4d99b8b2a825a2b43b3cfacb7ca76721b1a888b2
Sudeepa-R <sudeepar084@gmail.com>
2025-02-03 22:50:24 +0530

    clean command is added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const PORT = process.env.PORT || 9000;
+  await app.listen(PORT);
+  Logger.log(`Server running on http://localhost:${PORT}`);
 }
 bootstrap();
